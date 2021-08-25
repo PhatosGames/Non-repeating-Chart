7e76b3e4094df030bab8326b19096edcb1d3974f
Phatos <Phatos@DESKTOP-0T8J74T>
2021-08-25 13:18:54 +0200

    Commit 1#

---
+++ Non repeating Chart/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Non_repeating_Chart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            bool first = true;
+            List<char> charcaters = new List<char>();
+            List<char> repeated = new List<char>();
+
+
+            foreach (char c in input)
+            {
+                if (charcaters.Contains(c))
+                {
+                    repeated.Add(c);
+                }
+                charcaters.Add(c);
+             }
+
+            foreach(char c in charcaters)
+            {
+                if (!repeated.Contains(c))
+                {
+                    if (first)
+                    {
+                        Console.WriteLine($"{c.ToString()}");
+                        Console.WriteLine(string.Format("First unrepeated char:{0}", c.ToString()));
+                        first = false;
+                    }
+                }
+            }
+
+
+        }
+    }
+}
